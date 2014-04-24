a321f3b1d0bf37f1ed9964132d6dbf1a993b684c
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-24 11:11:25 -0300

    add route properties

--- app/models/route.js
+++ app/models/route.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 var routeSchema = mongoose.Schema({
+  from: { type: String, required: true },
+  to: { type: String, required: true },
+  distance: { type: Number, required: true }
 });
 
 var Route = mongoose.model('Route', routeSchema);
--- test/models/route_spec.js
+++ test/models/route_spec.js
@@ -2,9 +2,21 @@ var Route = require('../../app/models/route');
 
 describe(Route, function () {
   it('should validate presence of from', function (done) {
+    validatePresence('from', done);
+  });
+
+  it('should validate presence of to', function (done) {
+    validatePresence('to', done);
+  });
+
+  it('should validate presence of distance', function (done) {
+    validatePresence('distance', done);
+  });
+
+  function validatePresence(field, done) {
     new Route().save(function (err) {
-      //err.errors.from.should.be.an('object');
+      err.errors[field].should.be.an.Object;
       done();
     });
-  });
+  }
 });
