b4c30f5a277984f3a9c2811a7145065bf0f88aa6
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-24 22:01:43 -0300

    add basic graph functionality

---
+++ app/lib/graph.js
@@ -0,0 +1,73 @@
+var Graph = (function () {
+  function Graph() {
+    this.vertex = {};
+  }
+
+  Graph.prototype.addEdge = function (from, to, distance) {
+    this.connect(from, to, distance);
+    this.connect(to, from, distance);
+  }
+
+  Graph.prototype.connect = function (from, to, distance) {
+    this.vertex[from] = this.vertex[from] || {};
+    this.vertex[from][to] = distance;
+  }
+
+  Graph.prototype.shortestPath = function (from, to) {
+    var closed = [];
+    var open = [from];
+    var finished = false;
+    var data = {};
+
+    data[from] = {g: 0, f: 0, opened: true};
+
+    while (open.length) {
+      var i = -1;
+      var best = 1 << 30;
+
+      for (var j = 0; j < open.length; j++) {
+        var k = open[j];
+
+        if (data[k].f < best) {
+          best = data[k].f;
+          i = k;
+        }
+      }
+
+      best = open.splice(i, 1)[0];
+      data[best].closed = true;
+
+      if (best === to) {
+        return data[best].g;
+      }
+
+      var neighbors = Object.keys(this.vertex[best]);
+      for (i = 0; i < neighbors.length; i++) {
+        var neighbor = neighbors[i];
+        data[neighbor] = data[neighbor] || {};
+
+        if (data[neighbor].closed) continue;
+
+        var ng = data[best].g + this.vertex[best][neighbor];
+
+        if (!data[neighbor].opened || data[neighbor].g > ng) {
+          data[neighbor].g = ng;
+          data[neighbor].h = data[neighbor].h || this.vertex[best][neighbor] * (to.charCodeAt(0) - neighbor.charCodeAt(0));
+          data[neighbor].f = ng + data[neighbor].h;
+          data[neighbor].parent = best;
+
+          if (!data[neighbor].opened) {
+            data[neighbor].opened = true;
+            open.push(neighbor);
+          }
+        }
+      }
+    }
+
+    return -1;
+  };
+
+  return Graph;
+})();
+
+module.exports = Graph;
---
+++ test/lib/graph_spec.js
@@ -0,0 +1,53 @@
+var Graph = require('../../app/lib/graph');
+
+describe('Graph', function () {
+  it('should contain vertex', function () {
+    new Graph().vertex.should.be.an.Object;
+  });
+
+  context('#addEdge', function () {
+    it('should be a function', function () {
+      new Graph().addEdge.should.be.a.Function;
+    });
+
+    it ('should call connect twice', function () {
+      var graph = new Graph();
+      var stub = sinon.stub(graph, 'connect');
+
+      graph.addEdge('A', 'B', 10);
+
+      stub.calledTwice.should.be.ok;
+    });
+  });
+
+  context('#connect', function () {
+    it('should create the edge on graph\'s vertex', function () {
+      var graph = new Graph();
+
+      expect(graph.vertex.A).to.be.undefined;
+
+      graph.connect('A', 'B', 10);
+
+      graph.vertex.A.should.be.an.Object;
+      graph.vertex.A.B.should.be.equal(10);
+    });
+  });
+
+  context('#shortestPath', function () {
+    it('should be a function', function() {
+      new Graph().shortestPath.should.be.a.Function;
+    });
+
+    it('should solve a basic problem', function () {
+      var graph = new Graph();
+      graph.addEdge('A', 'B', 10);
+      graph.addEdge('B', 'D', 15);
+      graph.addEdge('A', 'C', 20);
+      graph.addEdge('C', 'D', 30);
+      graph.addEdge('B', 'E', 50);
+      graph.addEdge('D', 'E', 30);
+
+      graph.shortestPath('A', 'D').should.be.equal(25);
+    });
+  });
+});
