b9c0b643ef30671029ee1974d2e1b899ea96538c
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-28 07:09:49 -0300

    add log and port configuration to server

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express'),
     app = express(),
     routes = require('./app/routes'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    logger = require('morgan');
 
 mongoose.connect('mongodb://localhost/delivery');
 
-app.use(routes);
-app.listen(8080);
+port = process.argv[2] || 8080;
+
+app.use(logger())
+  .use(routes)
+  .listen(port);
+
+console.log('Server listening on http://localhost:%s', port);
