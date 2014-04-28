7a0a4aa9eaab698c5969d91eac17be5536fe74c0
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-28 13:06:13 -0300

    add require subvert to mock dependencies

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -9,9 +9,11 @@ FactoryGirl.definitionFilePaths = [__dirname + '/factories'];
 FactoryGirl.findDefinitions();
 
 chai.should();
+
 global.expect = chai.expect;
 global.FactoryGirl = FactoryGirl;
 global.sinon = sinon;
+global.RequireSubvert = require('require-subvert');
 global.mockRes = function(status, needle, done) {
   return {
     status: function (_status) {
